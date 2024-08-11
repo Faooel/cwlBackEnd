2fff2e4225d26149439af6edfb55ae4e6358c6e2
Benoit <giannini.benoit@gmail.com>
2024-08-11 20:30:25 +0200

    test

--- server.js
+++ server.js
@@ -12,7 +12,13 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Connexion à MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
 
 // Définir un simple endpoint
 app.get('/', (req, res) => {
